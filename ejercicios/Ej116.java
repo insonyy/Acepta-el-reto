70f0a7828c232b6395b9b68ab553e2180405e76b
insonyy <113542958+insonyy@users.noreply.github.com>
2022-12-13 17:35:10 +0100

    Add files via upload

---
+++ ejercicios/Ej116.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ej116 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num = teclado.nextInt();
+        for (int i = 0; i<num; i++){
+            System.out.println("Hola mundo");
+        }
+    }
+}
