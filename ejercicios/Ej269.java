db4f76026a4ff9ef74a906cfd89872ed3bff1e4b
insonyy <113542958+insonyy@users.noreply.github.com>
2022-12-23 19:13:32 +0100

    Add files via upload

---
+++ ejercicios/Ej269.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ej269 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int casos = s.nextInt();
+        int num, distancia, sum;
+
+        for (int i = 0; i < casos; i++) {
+
+            num = s.nextInt();
+            distancia = num;
+            sum = num;
+            while (num != 0) {
+                num = s.nextInt();
+                if (num != 0) {
+                    sum += num;
+                    distancia += sum;
+                }
+            }
+
+            System.out.println( distancia * 2 );
+        }
+
+    }
+}
